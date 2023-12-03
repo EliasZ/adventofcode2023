145f3fecb948568da59f4b8f83c7cd16427e51fa
Elias <eliaszerrouq@gmail.com>
2023-12-03 17:37:34 +0100

    day 3 part 2

--- src/main/java/it/saile/aoc2023/Day3.java
+++ src/main/java/it/saile/aoc2023/Day3.java
@@ -5,8 +5,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Day3 {
     public static void main(String[] args) throws Exception {
@@ -14,7 +16,7 @@ public class Day3 {
         var lines = Files.readAllLines(inputFile);
 
         var partNumbers = new ArrayList<PartNumber>();
-        var symbols = new HashMap<Integer, HashMap<Integer, Character>>();
+        var gearParts = new HashMap<Integer, HashMap<Integer, GearPart>>();
 
         int y = 0;
         for (String line : lines) {
@@ -25,12 +27,12 @@ public class Day3 {
                 char c = chars[i];
                 if (Character.isDigit(c)) {
                     number = Objects.requireNonNullElse(number, new StringBuilder()).append(c);
-                } else if (! Character.isDigit(c) && number != null) {
+                } else if (!Character.isDigit(c) && number != null) {
                     partNumbers.add(new PartNumber(number.toString(), y, i - number.length()));
                     number = null;
                 }
-                if (! Character.isDigit(c) && c != '.') {
-                    symbols.computeIfAbsent(y, k -> new HashMap<>()).put(i, c);
+                if (!Character.isDigit(c) && c != '.') {
+                    gearParts.computeIfAbsent(y, k -> new HashMap<>()).put(i, new GearPart(c, new HashSet<>(), y, i));
                 }
                 i++;
             } while (i < chars.length);
@@ -41,33 +43,58 @@ public class Day3 {
             y++;
         }
 
-        int total = 0;
 
         Set<Point> lookAround = Set.of(
-                new Point(-1 , 0), // up
-                new Point(1 , 0), // down
-                new Point(0 , -1), // left
-                new Point(0 , 1), // right
-                new Point(-1 , -1), // diagonal up left
-                new Point(-1 , 1), // diagonal up right
-                new Point(1 , -1), // diagonal down left
-                new Point(1 , 1) // diagonal down right
+                new Point(-1, 0), // up
+                new Point(1, 0), // down
+                new Point(0, -1), // left
+                new Point(0, 1), // right
+                new Point(-1, -1), // diagonal up left
+                new Point(-1, 1), // diagonal up right
+                new Point(1, -1), // diagonal down left
+                new Point(1, 1) // diagonal down right
         );
+
+        Set<GearPart> gears = new HashSet<>();
 
         for (PartNumber partNumber : partNumbers) {
             for (int i = 0; i < partNumber.number.length(); i++) {
                 final int x = i;
-                boolean found = lookAround.stream()
-                        .anyMatch(point -> symbols.getOrDefault(partNumber.y + point.x, new HashMap<>()).get(partNumber.x + x + point.y) != null);
-                if (found) {
-                    total += Integer.parseInt(partNumber.number);
-                    break;
-                }
+                Set<GearPart> collect = lookAround.stream()
+                        .map(point -> gearParts.getOrDefault(partNumber.y + point.x, new HashMap<>()).get(partNumber.x + x + point.y))
+                        .filter(Objects::nonNull)
+                        .filter(part -> part.c == '*')
+                        .collect(Collectors.toSet());
+                gears.addAll(collect);
+                collect.forEach(part -> part.adjacentTo.add(partNumber));
+
             }
         }
 
-        System.out.println(total);
+        int sumGearRatios = gears.stream()
+                .filter(g -> g.adjacentTo.size() == 2)
+                .reduce(0, (total, g) -> total + g.adjacentTo.stream()
+                                .map(p -> Integer.parseInt(p.number))
+                                .reduce(1, (a, b) -> a * b),
+                        Integer::sum);
+        System.out.println(sumGearRatios);
     }
 
-    record PartNumber(String number, int y, int x) {}
+    record PartNumber(String number, int y, int x) {
+    }
+
+    record GearPart(Character c, Set<PartNumber> adjacentTo, int y, int x) {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GearPart gearPart = (GearPart) o;
+            return y == gearPart.y && x == gearPart.x && Objects.equals(c, gearPart.c);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(c, y, x);
+        }
+    }
 }
